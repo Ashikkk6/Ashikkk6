050b4435250af82d05dd76acc7584fe07799b504
Ashikkk6 <144422050+Ashikkk6@users.noreply.github.com>
2023-09-08 12:46:54 +0600

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+-    I’m @Ashikkk6
+- 👀 I’m interested in Women 
+- 🌱 I’m currently learning MS EXEL 
+- 💞️ I’m looking to collaborate on International Company 
+- 📫 How to reach me Just Call Me
+
+<!---
+Ashikkk6/Ashikkk6 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
